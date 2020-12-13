4091c3eaf667cbfdfd5a7352632dba7d690d2f92
MT2314 <39105763+MT2314@users.noreply.github.com>
2020-12-12 20:16:51 -0500

    Intitial Files ++

---
+++ server.js
@@ -0,0 +1,21 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('public'));
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Basic route that sends the user first to the AJAX Page
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+
+app.get('/public/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
